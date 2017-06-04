4f955ec44ed2dc31745ca4c8eb26e1c301cf042d
dagothar <dagothar@gmail.com>
2017-06-04 20:44:23 +0200

    choosing rules

--- scripts/app.js
+++ scripts/app.js
@@ -30,6 +30,7 @@ define(['jquery', 'concrete', 'array2d'], function($, Concrete, array2d) {
     this.layer2 = undefined;
     this.interval = undefined;
     this.world = new array2d.Array2d(CONFIG.WORLD_WIDTH, CONFIG.WORLD_HEIGHT, 0);
+    this.rule = 0;
   };
 
 
@@ -52,6 +53,34 @@ define(['jquery', 'concrete', 'array2d'], function($, Concrete, array2d) {
     $(CONFIG.BTN_STOP_DIV).hide();
 
     /* configure callbacks */
+    $('#rule').on('input change', function() {
+      var r = parseInt($(this).val());
+
+      if (r & 1) $('[data-r=0]').removeClass('r0').addClass('r1'); else $('[data-r=0]').removeClass('r1').addClass('r0');
+      if (r & 2) $('[data-r=1]').removeClass('r0').addClass('r1'); else $('[data-r=1]').removeClass('r1').addClass('r0');
+      if (r & 4) $('[data-r=2]').removeClass('r0').addClass('r1'); else $('[data-r=2]').removeClass('r1').addClass('r0');
+      if (r & 8) $('[data-r=3]').removeClass('r0').addClass('r1'); else $('[data-r=3]').removeClass('r1').addClass('r0');
+      if (r & 16) $('[data-r=4]').removeClass('r0').addClass('r1'); else $('[data-r=4]').removeClass('r1').addClass('r0');
+      if (r & 32) $('[data-r=5]').removeClass('r0').addClass('r1'); else $('[data-r=5]').removeClass('r1').addClass('r0');
+      if (r & 64) $('[data-r=6]').removeClass('r0').addClass('r1'); else $('[data-r=6]').removeClass('r1').addClass('r0');
+      if (r & 128) $('[data-r=7]').removeClass('r0').addClass('r1'); else $('[data-r=7]').removeClass('r1').addClass('r0');
+
+      self.rule = r;
+    });
+
+    $('.r').click(function() {
+      var n = 1 << parseInt($(this).data('r'));
+      if ($(this).hasClass('r0')) {
+        $(this).removeClass('r0');
+        $(this).addClass('r1');
+        self.rule ^= n;
+      } else {
+        $(this).removeClass('r1');
+        $(this).addClass('r0');
+        self.rule ^= n;
+      }
+      $('#rule').val(self.rule);
+    });
   };
 
 
